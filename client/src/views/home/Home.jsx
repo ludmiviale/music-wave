3c6ff188d85c83485f8dffefe1db7cda26a2414e
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2024-03-18 11:41:21 -0300

    updating view Home

--- client/src/views/home/Home.jsx
+++ client/src/views/home/Home.jsx
@@ -1,11 +1,26 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCategories, getSongs } from "../../redux/actions";
 import Nav from "../../components/nav/Nav";
 import Banner from "../../components/banner/Banner";
+import Cards from "../../components/cards/Cards";
+import "./home.css";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const { allSongs, allCategories } = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch(getSongs());
+    dispatch(getCategories());
+  }, []);
+
   return (
     <div>
       <Nav />
       <Banner />
+      <h2 className="category">Soft Rock</h2>
+      <Cards allSongs={allSongs} />
     </div>
   );
 };
