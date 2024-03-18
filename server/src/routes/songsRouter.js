f63b76aabfb4c0b5521cadbabf86987ebe10c4f6
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2024-03-18 11:42:06 -0300

    updating routes

--- server/src/routes/songsRouter.js
+++ server/src/routes/songsRouter.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 const songsRouter = Router();
 const {
   getSongsHandler,
+  getSongByIdHandler,
   createSongHandler,
 } = require("../handlers/songsHandler");
 
 songsRouter.get("/", getSongsHandler);
+songsRouter.get("/:id", getSongByIdHandler);
 songsRouter.post("/", createSongHandler);
 
 module.exports = songsRouter;
