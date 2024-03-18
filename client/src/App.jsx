dfc67b3c0abf65f03880b28846446392449b249c
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2024-03-18 11:42:20 -0300

    updating App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./views/home/Home";
+import Detail from "./views/detail/Detail";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
       </Routes>
     </div>
   );
